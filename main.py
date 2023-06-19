ccba4eabe6c19c3137fd1b16ad542351a5ca68d9
ilmir112 <Zufaroff112@gmail.com>
2023-06-19 15:30:38 +0500

    add file

---
+++ main.py
@@ -0,0 +1,12 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+
+class QaranApp(MDApp):
+    def build(self):
+        return MDLabel(text="Hello, World", halign="center")
+
+
+QaranApp().run()
+
+
