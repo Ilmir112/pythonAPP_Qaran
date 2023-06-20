f877dc289bdd03c243fc3f508f3257ed4210508d
ilmir112 <Zufaroff112@gmail.com>
2023-06-20 11:29:07 +0500

    Auto build APK-File 32bit, 64bit

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivy.lang import Builder
 
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.app import MDApp
+
+class ContentNavigationDrawer(MDBoxLayout):
+    pass
 
 class QaranApp(MDApp):
-    def build(self):
-        return MDLabel(text="Hello, World", halign="center")
 
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        screen = Builder.load_file('qaran.kv')
+
+        return screen
 
 
 QaranApp().run()
-
-
