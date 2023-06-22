df1481f37e92bbdb9e9deb5122bfbf7cf931e8eb
ilmir112 <Zufaroff112@gmail.com>
2023-06-22 09:05:55 +0500

    Auto build APK-File 32bit, 64bit

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from kivy.lang import Builder
 
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.menu import MDDropdownMenu
 from kivymd.app import MDApp
 
-class ContentNavigationDrawer(MDBoxLayout):
-    pass
+
+
 
 class QaranApp(MDApp):
-
     def build(self):
+        menu_items = [{'text': 'Меню левое 1', 'viewclass': 'OneLineListItem', 'on_release': lambda x = 'меню левое 1': self.menu_callback(x),},
+                      {'text': 'Меню левое 2', 'viewclass': 'OneLineListItem', 'on_release': lambda x = 'меню левое 2': self.menu_callback(x), },
+                      {'text': 'Меню левое 1', 'viewclass': 'OneLineListItem', 'on_release': lambda x = 'меню левое 1': self.menu_callback(x), },]
+        self.menu = MDDropdownMenu(items=menu_items, width_mult=2.5, )
+
+
+
         self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Teal"
         screen = Builder.load_file('qaran.kv')
 
         return screen
