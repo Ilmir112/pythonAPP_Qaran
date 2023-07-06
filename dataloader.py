f162b78063405521f4fca0b612c4294028988bdf
ilmir112 <Zufaroff112@gmail.com>
2023-07-06 16:34:02 +0500

    new createrndom answer

--- dataloader.py
+++ dataloader.py
@@ -5,20 +5,16 @@ from collections import defaultdict
 
 
 # Function importing data from CSV file
-def data_from_csv(question, firstanswer, secondanswer, thirdanswer, fourthanswer, rightanswer):
+def data_from_csv(question):
     with open("data/Questions.csv", "r", encoding = 'utf-8') as file:
         csv_data = csv.reader(file)
         for row in csv_data:
             # Assigning each row item to a specific category
-            number, text,  first, second, third, fourth, right = row
+            #text, rightanswer = row
             # Creating Python dictionary with this data
-            question.append(text)
-            firstanswer.append(first)
-            secondanswer.append(second)
-            thirdanswer.append(third)
-            fourthanswer.append(fourth)
-            rightanswer.append(right)
-        return question, firstanswer, secondanswer, thirdanswer, fourthanswer, rightanswer
+            question.append(row)
+
+    return question
 
 
 
--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from kivymd.uix.menu import MDDropdownMenu
 from kivy.properties import ObjectProperty, ListProperty
 from kivymd.app import MDApp
 
-
 import dataloader as dt
 import random
+
 
 class ContentNavigationDrawer(MDBoxLayout):
     screen_manager = ObjectProperty()
@@ -18,59 +18,76 @@ class ContentNavigationDrawer(MDBoxLayout):
 
 
 question = []
-firstanswer = []
-secondanswer = []
-thirdanswer = []
-fourthanswer = []
-rightanswer = []
 
+question_list = dt.data_from_csv(question)
 
-question_list = dt.data_from_csv(question, firstanswer, secondanswer, thirdanswer, fourthanswer, rightanswer)
 
 class SignButton(Button):
-    bg_color = ListProperty([1, 1, 1, 0])
+    bg_color = ListProperty([1, 0, 1, 0])
+
 
 class OptionButton(Button):
     bg_color = ListProperty([1, 1, 1, 0])
 
-class QaranApp(MDApp):
 
-    selected_sign = ''
+class QaranApp(MDApp):
+    selected_game = ''
+
     def build(self):
         global screen_manager
-        screen_manager = ScreenManager(transition = NoTransition())
+        screen_manager = ScreenManager(transition=NoTransition())
         screen_manager.add_widget(Builder.load_file('main.kv'))
-        screen_manager.add_widget(Builder.load_file('select_sign.kv'))
+        screen_manager.add_widget(Builder.load_file('select_game.kv'))
         screen_manager.add_widget(Builder.load_file('quiz.kv'))
         # screen_manager.add_widget(Builder.load_file('final_score.kv'))
 
-        menu_items = [{'text': 'Меню левое 1', 'viewclass': 'OneLineListItem', 'on_release': lambda x = 'меню левое 1': self.menu_callback(x),},
-                      {'text': 'Меню левое 2', 'viewclass': 'OneLineListItem', 'on_release': lambda x = 'меню левое 2': self.menu_callback(x), },
-                      {'text': 'Меню левое 1', 'viewclass': 'OneLineListItem', 'on_release': lambda x = 'меню левое 1': self.menu_callback(x), },]
+        menu_items = [{'text': 'Меню левое 1', 'viewclass': 'OneLineListItem',
+                       'on_release': lambda x='меню левое 1': self.menu_callback(x), },
+                      {'text': 'Меню левое 2', 'viewclass': 'OneLineListItem',
+                       'on_release': lambda x='меню левое 2': self.menu_callback(x), },
+                      {'text': 'Меню левое 1', 'viewclass': 'OneLineListItem',
+                       'on_release': lambda x='меню левое 1': self.menu_callback(x), }, ]
         self.menu = MDDropdownMenu(items=menu_items, width_mult=2.5, )
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Teal"
 
         return screen_manager
 
-    def select_sign(self, sign):
-       self.selected_sign = sign
+    def select_game(self, game):
+        self.selected_game = game
 
-       question1 = random.choice(question)
-       get_answer1 = random.choice(firstanswer)
-       get_answer2 = random.choice(secondanswer)
-       get_answer3 = random.choice(thirdanswer)
-       get_answer4 = random.choice(fourthanswer)
+        question_random = random.choice(question)
+        guestion1 = question_random[0]
+        self.right_answer = question_random[1]
+        answer_list = [self.right_answer]
 
-       screen_manager.get_screen('quiz').ids.question.text = f'{question1}'
-       screen_manager.get_screen('quiz').ids.answer1.text = f'{get_answer1}'
-       screen_manager.get_screen('quiz').ids.answer2.text = f'{get_answer2}'
-       screen_manager.get_screen('quiz').ids.answer3.text = f'{get_answer3}'
-       screen_manager.get_screen('quiz').ids.answer4.text = f'{get_answer4}'
+        while len(answer_list) != 4:
+            answer_random = random.choice(question)[1]
+            if answer_random not in answer_list:
+                answer_list.append(answer_random)
 
-       screen_manager.current = 'quiz'
+        random.shuffle(answer_list)
+
+        screen_manager.get_screen('quiz').ids.question.text = f'{guestion1}'
+
+        for i in range(1, 5):
+            screen_manager.get_screen('quiz').ids[f'answer{i}'].text = f'{answer_list[i - 1]}'
+
+        screen_manager.correct = 'quiz'
+
+    def get_id(self, instance):
+        for id, widget in instance.parent.parent.parent.items():
+            if widget.__self__ == instance:
+                return id
+
+    def quiz(self, answer, instance):
 
 
+        if answer == self.right_answer:
+            screen_manager.get_screen('quiz').ids[self.get_id(instance)].bg_color = (0, 1, 0, 1)
+
+    def next_question(self):
+        pass
 
 
 QaranApp().run()
