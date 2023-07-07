69807128cab0dc04cb2721f09b093cb1e7861780
ilmir112 <Zufaroff112@gmail.com>
2023-07-07 12:43:27 +0500

    новый проект с нуля

--- main.py
+++ main.py
@@ -1,37 +1,27 @@
-from kivy.lang import Builder
+import button as button
 from kivy.core.text import LabelBase
+from kivy.properties import ListProperty
 from kivy.uix.button import Button
-from kivy.uix.screenmanager import ScreenManager, NoTransition, TransitionBase
-
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.menu import MDDropdownMenu
-from kivy.properties import ObjectProperty, ListProperty
+from kivy.uix.screenmanager import ScreenManager, NoTransition
 from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+Window.size = (350, 600)
 
-import dataloader as dt
 import random
-
-
-class ContentNavigationDrawer(MDBoxLayout):
-    screen_manager = ObjectProperty()
-    nav_drawer = ObjectProperty()
-
-
-question = []
-
-question_list = dt.data_from_csv(question)
-
-
-class SignButton(Button):
-    bg_color = ListProperty([1, 0, 1, 0])
-
-
-class OptionButton(Button):
+import dataloader as dt
+class SelectGameButton(Button):
     bg_color = ListProperty([1, 1, 1, 0])
 
+class OptionButton(Button):
+    bg_color = ListProperty([1, 1, 1, 1])
 
+question = []
+question_list = dt.data_from_csv(question)
 class QaranApp(MDApp):
+
     selected_game = ''
+    right_answer = ''
 
     def build(self):
         global screen_manager
@@ -39,23 +29,11 @@ class QaranApp(MDApp):
         screen_manager.add_widget(Builder.load_file('main.kv'))
         screen_manager.add_widget(Builder.load_file('select_game.kv'))
         screen_manager.add_widget(Builder.load_file('quiz.kv'))
-        # screen_manager.add_widget(Builder.load_file('final_score.kv'))
-
-        menu_items = [{'text': 'Меню левое 1', 'viewclass': 'OneLineListItem',
-                       'on_release': lambda x='меню левое 1': self.menu_callback(x), },
-                      {'text': 'Меню левое 2', 'viewclass': 'OneLineListItem',
-                       'on_release': lambda x='меню левое 2': self.menu_callback(x), },
-                      {'text': 'Меню левое 1', 'viewclass': 'OneLineListItem',
-                       'on_release': lambda x='меню левое 1': self.menu_callback(x), }, ]
-        self.menu = MDDropdownMenu(items=menu_items, width_mult=2.5, )
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Teal"
-
+        #screen_manager.add_widget(Builder.load_file('final_score.kv'))
         return screen_manager
 
     def select_game(self, game):
         self.selected_game = game
-
         question_random = random.choice(question)
         guestion1 = question_random[0]
         self.right_answer = question_random[1]
@@ -71,23 +49,18 @@ class QaranApp(MDApp):
         screen_manager.get_screen('quiz').ids.question.text = f'{guestion1}'
 
         for i in range(1, 5):
-            screen_manager.get_screen('quiz').ids[f'answer{i}'].text = f'{answer_list[i - 1]}'
+            screen_manager.get_screen('quiz').ids[f'option{i}'].text = f'{answer_list[i - 1]}'
 
-        screen_manager.correct = 'quiz'
+        screen_manager.current ='quiz'
 
     def get_id(self, instance):
-        for id, widget in instance.parent.parent.parent.items():
-            if widget.__self__ == instance:
-                return id
+        print(instance.parent.parent.parent)
 
-    def quiz(self, answer, instance):
-
-
-        if answer == self.right_answer:
-            screen_manager.get_screen('quiz').ids[self.get_id(instance)].bg_color = (0, 1, 0, 1)
+    def quiz(self, option, instance):
+        print(option, self.get_id(instance))
 
     def next_question(self):
         pass
 
-
-QaranApp().run()
+if __name__ == '__main__':
+    QaranApp().run()
